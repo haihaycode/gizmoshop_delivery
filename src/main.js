44af48a1200b9de18338962b6e298ef61b3d7655
thansex000 <loclop3@gmail.com>
2024-10-07 19:54:38 +0700

    multipart layout

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/css/tailwind.css'
+import router from './router'
 
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.use(router)
+app.mount('#app')
